a3fb71d83ee9e7f12d8bdc730e1512cadfdfb3e7
apla02 <1873@holbertonschool.com>
2020-06-12 00:45:23 +0000

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphab.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+ 
+/**
+  * main - alphabeth
+  * Return: Always 0 (Success)
+  */
+ 
+int main(void)
+{
+	char alph = 'a';
+	     
+	while (alph <= 'z')
+	{
+	if (alph != 'e' && alph != 'q')
+	{
+	putchar(alph);
+	}
+	alp++;
+	}
+	putchar('\n');
+	return (0);
+}
