52e94201887eb2ea54c490a459be396848795258
apla02 <1873@holbertonschool.com>
2020-06-15 23:54:13 +0000

    holberton.h

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,20 @@
-#include "holberton.h"
 #include <stdio.h>
+#include "holberton.h"
 /**
-  * main- protocol holberton school 
+  * main- protocol holberton school
   * Return: always zero
   */
 int main(void)
 {
-	_putchar('Holberton');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
