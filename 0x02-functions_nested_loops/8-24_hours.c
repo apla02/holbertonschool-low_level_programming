a23c180df4463bd69e978d1d538cc3b73d9c462e
apla02 <1873@holbertonschool.com>
2020-06-16 21:52:56 +0000

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
 /**
- * jack_bauer - count hour and minutes
+ * jack_bauer - count hours and minutes
  *
  * Return: always 0.
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int i;
+	int j;
+	char del = ':';
 
 	for (i = 00; i <= 23; i++)
 	{
-		for (j = i; j <= 59; j++)
+		for (j = 00 ; j <= 59; j++)
 		{
 			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
-			_putchar(':');
+			_putchar(del);
 			_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 			_putchar('\n');
