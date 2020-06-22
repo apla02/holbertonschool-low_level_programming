f329b1da84e458d1a56863fd1f367ce0e2408b85
apla02 <1873@holbertonschool.com>
2020-06-22 22:15:07 +0000

    a and b swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int - function to reset n to 98
+ * @a: first integer
+ * @b: second integer
+ * void: no content
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
