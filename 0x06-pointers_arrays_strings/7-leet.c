91b7c3251664df7d3a001f3c089fb2140332fcfe
apla02 <1873@holbertonschool.com>
2020-06-26 21:22:24 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+* leet -  function that encripts
+* @s: string
+* Return: no zero
+*/
+char *leet(char *s)
+{
+
+	int i;
+	int j = 0;
+	char a[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
+
+	while (s[i] != '\0')
+	{
+		while (a[j] != '\0')
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = n[j];
+			}
+		j++;
+		}
+		i++;
+	}
+	return (s);
+}
