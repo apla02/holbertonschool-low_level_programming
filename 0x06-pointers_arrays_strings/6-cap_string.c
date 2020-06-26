d59381d7817552c914cf23606ddf4d6fdd28dfb7
apla02 <1873@holbertonschool.com>
2020-06-26 02:19:32 +0000

    fix

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+* cap_string -  function to change a lowercase char to uppercase one
+* @s: string
+* Return: void
+*/
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] >= 97 && s[i] <= 122))
+		{
+			s[i] = s[i] - 32;
+		}
+		else
+		{
+			s[i] = s[i];
+		}
+		i++;
+	}
+		return (s);
+}
