2561cd06e84d187024d21b08296a7928abb6ee22
apla02 <1873@holbertonschool.com>
2020-06-30 02:46:11 +0000

    diagonales

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* print_chessboard - function for print a chessboard
+* @a: pointer to chess
+* Return: void
+*/
+void print_diagsums(int *a, int size)
+{
+	int i, suma1 = 0, suma2 = 0, m;
+
+	m = size * size;
+	for (i = 0; i < m; i++)
+	{ 
+			if (i % (size + 1) == 0)
+			{
+				suma1 += a[i];
+			}
+	}
+	for (i = m - size ; i > 0; i--)
+	{
+		if ( i % (size - 1) == 0)
+		{
+			suma2 += a[i];
+		}
+	}
+	printf("%d, %d\n", suma1, suma2);
+}
