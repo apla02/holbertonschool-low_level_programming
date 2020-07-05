93028040a849552a75a3f03729684d0c00732042
apla02 <1873@holbertonschool.com>
2020-07-05 15:58:10 +0000

    program -1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
 * main - main function
 * @argc: number of arguments to program
 * @argv: vector of arguments
 * Return: always zero.
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{
-	printf("%s\n", argv[i]);
-	}
+	{}
+	printf("%d\n", i - 1);
 	return (0);
 }
