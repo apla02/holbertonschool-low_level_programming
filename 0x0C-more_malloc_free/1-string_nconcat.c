b735ee53de74ff53ccf5b9af7d5240624fccc838
apla02 <1873@holbertonschool.com>
2020-07-09 20:32:06 +0000

    strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,13 +27,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 	s3 = malloc((len1 + len2 + 1) * sizeof(char));
 	if (s3 == NULL)
-		{
-			return (NULL);
-		}
+	{
+		return (NULL);
+	}
 	for (len1 = 0; *(s1 + len1); len1++)
-		{
-			*(s3 + len1) = *(s1 + len1);
-		}
+	{
+		*(s3 + len1) = *(s1 + len1);
+	}
 	for (len2 = 0; len2 < n && *(s2 + len2); len1++, len2++)
 		{
 		*(s3 + len1) = *(s2 + len2);
