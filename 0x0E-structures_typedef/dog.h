a8142486a638d4403c37317300012aa35b589593
apla02 <1873@holbertonschool.com>
2020-07-13 18:21:39 -0500

    dog incluir siempre directivas de la estructura

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_
+#define _DOG_
+/**
+ * struct dog - my structur for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
