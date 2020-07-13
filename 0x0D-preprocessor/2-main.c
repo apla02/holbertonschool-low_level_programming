7e45fb1f73580eae2572a7e5bc582b3da169fe62
apla02 <1873@holbertonschool.com>
2020-07-13 16:15:06 -0500

    repair FILE

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - function
- * /
+ * Return : always zero
+ */
  int main(void)
  {
-     printf("s\n", _FILE_);
+     printf("%s\n", __FILE__);
      return (0);
  }
