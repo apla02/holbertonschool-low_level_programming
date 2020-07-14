a6dd5803e424634044ec0baec315ca738d29f9b8
apla02 <1873@holbertonschool.com>
2020-07-13 20:34:40 -0500

    fix spaces in printf

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		d->name = ("nil");
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner = ("nil");
-	printf(" name: %s\n age: %f\n owner: %s\n", d->name, d->age, d->owner);
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
