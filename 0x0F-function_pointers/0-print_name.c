a09c92a5d082bb174ba8c887e386718dc36826ca
apla02 <1873@holbertonschool.com>
2020-07-15 15:21:11 -0500

    print

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - function ti print a name
+ * @name: name to print
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		(*f)(name);
+	}
+}
