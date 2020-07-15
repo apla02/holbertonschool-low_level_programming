17b9391a18a78a3a145b9a86ee2198d4c9f2504c
apla02 <1873@holbertonschool.com>
2020-07-15 15:26:53 -0500

    fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,4 @@
-#ifndefine _POINTER_
+#ifndef _POINTER_
 #define _POINTER_
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
 #endif
