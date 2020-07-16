8e68c77382fd275cd4a7a3a607ad877f7b122832
apla02 <1873@holbertonschool.com>
2020-07-16 14:22:47 -0500

    calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function argc and argv
+ * @argc: number of arguments
+ * @argv: vector or punters of arrays
+ * Return: always zero
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	operation = get_op_func(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (((operation == op_div) || (operation == op_mod)) && (b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	c = operation(a, b);
+	printf("%d\n", c);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,41 @@
+#include "calc.h"
+#include <stdio.h>
+/**
+ * op_add - adds two integers
+ * @a: integer
+ * @b: integer
+ * Return: no zero
+ */
+int op_add(int a, int b) {return (a + b); }
+
+/**
+ * op_sub - substract two integers
+ * @a: int
+ * @b: int
+ * Return: no zero
+ */
+int op_sub(int a, int b) {return (a - b); }
+
+/**
+ * op_mul - multiply two integers
+ * @a: int
+ * @b: int
+ * Return: no zero
+*/
+
+int op_mul(int a, int b) {return (a * b); }
+/**
+ * op_div - divide two integers
+ * @a: int
+ * @b: int
+ * Return: no zero
+ */
+int op_div(int a, int b) {return (a / b); }
+
+/**
+ * op_mod - remind between two integers
+ * @a: int
+ * @b: int
+ * Return: no zero
+ */
+int op_mod(int a, int b) {return (a % b); }
