0c432fff7c8a2230396f999f665feb2f0ab0b08d
apla02 <1873@holbertonschool.com>
2020-07-18 12:46:10 -0500

    fix betty

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,14 +13,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list print_s;
 
 	va_start(print_s, n);
-	
 	for (i = 0; i < n; i++)
 	{
 		printf("%s", va_arg(print_s, char *));
 		if (i != n - 1)
-			printf("%s", separator);
-		if (va_arg(print_s, char *) == '\0')
-			printf("nil");
+		printf("%s", separator);
 	}
 	printf("\n");
 	va_end(print_s);
