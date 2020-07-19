10df82cecac826e2d555633ea32c427de0c1cae0
apla02 <1873@holbertonschool.com>
2020-07-19 12:25:30 -0500

    fix spaces

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,7 +7,7 @@
 */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
+	unsigned int i = 0, j = 0;
 	char *str;
 	va_list flist;
 
@@ -18,15 +18,15 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", va_arg(flist, int));
-				printf(", ");
+				j = 1;
 				break;
 			case 'i':
 				printf("%d", va_arg(flist, int));
-				printf(", ");
+				j = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(flist, double));
-				printf(", ");
+				j = 1;
 				break;
 			case 's':
 				str = va_arg(flist, char *);
@@ -36,8 +36,11 @@ void print_all(const char * const format, ...)
 					printf("%s", str);
 				break;
 			default:
+				j = 0;
 				break;
 		}
+		if (j == 1 && format[i + 1] != '\0')
+			printf(", ");
 	i++;
 	}
 	va_end(flist);
