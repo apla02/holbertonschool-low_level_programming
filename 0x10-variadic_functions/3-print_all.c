d1162138de9470df6f830a30c3a9c0c6394640be
apla02 <1873@holbertonschool.com>
2020-07-18 19:36:10 -0500

    new file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,65 +2,44 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * print_c - fnction to print a format
- * @c: typef
-*/
-void print_c(va_list c) {printf("%c", va_arg(c, int)); }
-/**
- * print_i - fnction to print a format
- * @i: typef
-*/
-void print_i(va_list i) {printf("%d", va_arg(i, int)); }
-/**
- * print_f - fnction to print a format
- * @f: typef
-*/
-void print_f(va_list f) {printf("%f", va_arg(f, double)); }
-/**
- * print_s - fnction to print a format
- * @s: typef
-*/
-void print_s(va_list s)
-{
-	char *string;
-
-	string =  va_arg(s, char *);
-	if (string == NULL)
-		printf("(nil)");
-	printf("%s", string);
-}
-/**
  * print_all - function to print a format
  * @format: type format parameter
 */
 void print_all(const char * const format, ...)
 {
-	unsigned int i, j;
-	char *separator;
-	va_list args;
-	format_t printall[] = {
-	{"c", print_c},
-	{"i", print_i},
-	{"f", print_f},
-	{"s", print_s}
-	};
-	separator = "";
-	va_start(args, format);
-	i = 0;
-	while (format && format[i])
+	unsigned int i = 0;
+	char *str;
+	va_list flist;
+
+	va_start(flist, format);
+	while (format[i])
 	{
-		j = 0;
-		while (j < 4)
+		switch (format[i])
 		{
-		if (format[i] == *printall[j].typef)
-		{
-		printf("%s", separator);
-		printall[j].f(args);
-		separator = ", ";
-		}
-		j++;
+			case 'c':
+				printf("%c", va_arg(flist, int));
+				printf(", ");
+				break;
+			case 'i':
+				printf("%d", va_arg(flist, int));
+				printf(", ");
+				break;
+			case 'f':
+				printf("%f", va_arg(flist, double));
+				printf(", ");
+				break;
+			case 's':
+				str = va_arg(flist, char *);
+				if (str == NULL)
+					printf("(nil)");
+				else
+					printf("%s", str);
+				break;
+			default:
+				break;
 		}
 	i++;
 	}
+	va_end(flist);
 	printf("\n");
 }
