9f68aaddc46d82e8a8cc77244e45b83e691fc9ce
apla02 <1873@holbertonschool.com>
2020-07-23 16:12:58 -0500

    print the linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - print a linked list
+ * @h: pointer to structure
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	if (h == 0)
+		return (0);
+
+	while (h != '\0')
+	{
+		if ((*h).str != '\0')
+			printf("[%u] %s\n", (*h).len, (*h).str);
+		else
+			{
+				printf("[0] (nill)\n");
+			}
+		h = (*h).next;
+		i++;
+	}
+	return (i);
+}
