87bb6d087b4d9c64c6ddc9a5bac4d4efcf4b9924
apla02 <1873@holbertonschool.com>
2020-07-23 18:23:28 -0500

    new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_node - print a linked list
+ * @head: double pointer to structure
+ * @str: string
+ * Return: the number of nodes
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = NULL;
+	int i = 0;
+
+	if (head == '\0' || str == '\0')
+		return (0);
+	new = (list_t *)malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	while (*(str + i))
+	{
+		i++;
+	}
+	(*new).str = strdup(str);
+	(*new).len = i;
+	(*new).next = *head;
+	*head = new;
+	return (new);
+}
