f76d59a7e6b99bc4d920df3625a55561708cd094
apla02 <1873@holbertonschool.com>
2020-07-27 17:03:54 -0500

    free space with a double pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint- free a listint_t list.
+ * @head: pointer to pointer the first element of listint_t.
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr = NULL;
+
+	while (*head)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
+	}
+	*head = NULL;
+}
