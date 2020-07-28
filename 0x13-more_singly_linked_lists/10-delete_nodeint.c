8568d32ec25bdc138f0a533faff05427c0f05499
apla02 <1873@holbertonschool.com>
2020-07-28 15:55:09 -0500

    fix len

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * listint_len - print all the elements of a listint_t list.
+ * @h: pointer to strcutur listint_t
+ * Return: the numbers of nodes
+ */
+unsigned int len(listint_t *h)
+{
+	unsigned int index = 0;/* counts the number of nodes*/
+
+	if (h == '\0')
+		return (0);
+
+	while (h)
+	{
+		h = h->next;/*update the adress of pointer to h->next */
+		index++;
+	}
+	return (index);
+}
+/**
  * delete_nodeint_at_index - delete a  node in an index the listint_t.
  * @head: pointer  to the strucure.
  * @index: index of listint_t list.
@@ -7,9 +26,10 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
+	unsigned int len_n;
+	len_n = len(*head);
 
-	if (head && *head)
+	if (head && *head && index <= len_n)
 	{
 		listint_t *siguiente;
 		listint_t *anterior;
@@ -21,6 +41,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		else
 		{
+			unsigned int i = 0;
+
 			siguiente = *head;
 			while (i++ < index && (siguiente->next))
 			{
@@ -29,8 +51,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			}
 			anterior->next = siguiente->next;
 		}
-			free(siguiente);
-			return (1);
+		free(siguiente);
+		return (1);
 	}
 	else
 		return (-1);
