c2f29ad82009600eb19bcf4c14de55631bec9aae
apla02 <1873@holbertonschool.com>
2020-07-31 11:49:30 -0500

    counting bits in 2 variables  ^

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,31 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "holberton.h"
-/**
- * print_binary- function print a binary
- * @n:  a number int
- * Return: void.
- */
-void print_binary(unsigned long int n)
-{
-	unsigned long int
-	if (n > 1)
-		print_binary(n >> 1);
-
-	binary = ((n & 1) + '0');
-
-}
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
  * flip_bits - function set a bit at index
@@ -35,12 +10,15 @@ int _putchar(char c)
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int bit_n = 0;
+	unsigned long int i = 0;
+	unsigned long int suma = 0;
 
-	print_binary(n);
-	print_binary(m);
 
-	bit_n = m ^ n;
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
 
-	return (bit_n);
+		if ((((m ^ n) >> i) & 1) == 1)
+			suma = suma + 1;
+	}
+	return (suma);
 }
