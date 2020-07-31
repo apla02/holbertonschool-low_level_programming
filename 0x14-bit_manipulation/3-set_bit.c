877bd6a5d71505443e618f69e74060d9bb29a7bd
apla02 <1873@holbertonschool.com>
2020-07-30 19:34:17 -0500

    confused about the logic

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * set_bit - function set a bit at index
+ * @n:  a number int
+ * @index: index
+ * Return: void.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (index < (sizeof(unsigned long int) * 8) && *n)
+	{
+		*n |= (1 << index);
+		return (1);
+	}
+	else
+		return (-1);
+}
