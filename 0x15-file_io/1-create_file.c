8b973888e246071594c4308f653a54b403bec8e9
apla02 <1873@holbertonschool.com>
2020-08-04 18:13:33 -0500

    add new flag

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,13 +16,13 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == 0)
+	if (text_content == NULL)
 		return (1);
 	while (text_content[size])
 	{
 		size++;
 	}
-	writ = write(STDOUT_FILENO, text_content, size);
+	writ = write(fd, text_content, size);
 	if (writ == -1)
 		return (-1);
 
