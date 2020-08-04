95b145837fb968d98f130255b7aaa6ca7fcec8f8
apla02 <1873@holbertonschool.com>
2020-08-04 18:53:00 -0500

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * append_text_to_filez - function to create a file
+ * append_text_to_file- function to create a file
  * @filename: file
  * @text_content: number of letters of the file
  * Return: numbers of letters or zero it fails
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
