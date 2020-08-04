66157085499853aa2db75380e00063f3b4d6a051
apla02 <1873@holbertonschool.com>
2020-08-04 18:04:58 -0500

    fix line 29

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	buffer = malloc(sizeof(char) * (size));
 	if (buffer == NULL)
 		return (0);
-	writ = write(STDOUT_FILENO, buffer, size);
+	writ = write(STDOUT_FILENO, text_content, size);
 	if (writ == -1)
 	{
 		free(buffer);
