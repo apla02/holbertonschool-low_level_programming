24afcec52999a0c7b9abcdfe0d59a7113f14823c
apla02 <1873@holbertonschool.com>
2020-08-04 18:01:26 -0500

    fix the line 17

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,15 @@ int create_file(const char *filename, char *text_content)
 	int fd = 0;
 	char *buffer;
 	int size = 0;
-	int  byt_read;
+	int  writ;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 	while (text_content[size])
 	{
 		size++;
@@ -24,8 +26,8 @@ int create_file(const char *filename, char *text_content)
 	buffer = malloc(sizeof(char) * (size));
 	if (buffer == NULL)
 		return (0);
-	byt_read = read(fd, buffer, size);
-	if (byt_read == -1)
+	writ = write(STDOUT_FILENO, buffer, size);
+	if (writ == -1)
 	{
 		free(buffer);
 		return (-1);
