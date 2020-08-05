c82a65e3d78006bf13a7552848397a7b081806bf
apla02 <1873@holbertonschool.com>
2020-08-05 17:13:34 -0500

    fix the loop for len

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,43 +1,39 @@
 #include "holberton.h"
+#define BUFFLEN (1024)
 /**
- * main- function to create a file
- * @argc: file
- * @argv: number of letters of the file
+ * main- function av y av
+ * @ac: file
+ * @av: number of letters of the file
  * Return: numbers of letters or zero it fails
  */
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
-	int fd_from;
-	int fd_to;
-	int read_from;
-	int write_to;
-	char buffer[1024];
+	int fd_from, fd_to, read_from, write_to, close_from, close_to;
+	char buffer[BUFFLEN];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fd_from = open(argv[1], O_RDONLY);
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd_to = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd_to = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	while ((read_from = read(fd_from, buffer, BUFFLEN) != 0))
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		if (read_from == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		write_to = write(STDOUT_FILENO, buffer, read_from);
+		if (write_to == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
-	read_from = read(fd_from, buffer, 1024);
-	write_to = write(STDOUT_FILENO, buffer, read_from);
-	if (write_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	close(fd_from);
-	close(fd_to);
+		write_to = write(STDOUT_FILENO, buffer, read_from);
+
+	close_from = close(fd_from);
+	if (close_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd_from), exit(100);
+	close_to = close(fd_to);
+		if (close_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd_to), exit(100);
 	return (0);
 }
