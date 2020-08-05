6415ee3a57b7799bbd316fa586a830de7567887b
apla02 <1873@holbertonschool.com>
2020-08-05 17:42:13 -0500

    while control

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,9 +25,12 @@ int main(int ac, char *av[])
 		read_from = read(fd_from, buffer, BUFFLEN);
 		if (read_from == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		if (read_from < BUFFLEN)
+			write_to = write(STDOUT_FILENO, buffer, read_from);
+			if (write_to == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+			break;
 		write_to = write(STDOUT_FILENO, buffer, read_from);
-		if (write_to == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 
 	close_from = close(fd_from);
