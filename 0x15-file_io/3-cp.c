ed878e7a298ca9636b5ebf9e378c27dbc405b689
apla02 <1873@holbertonschool.com>
2020-08-07 12:02:20 -0500

    loop again

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,12 +20,13 @@ int main(int ac, char *av[])
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	while (read_from = read(fd_from, buffer, BUFFLEN))
+	while (1)
 	{
-		if (read_from == -1)
+		read_from = read(fd_from, buffer, BUFFLEN);
+		if (read_from < 0)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		write_to = write(STDOUT_FILENO, buffer, read_from);
-		if (write_to == -1)
+		if (write_to < 0)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 
