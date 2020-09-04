08fa95e9b9c9f566890b390b3dfdea3b06b2cb59
apla02 <1873@holbertonschool.com>
2020-09-04 13:58:03 -0500

    free a doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - function that free a dlistint_t list.
+ * @head: double pointer to structure
+ * Return: none
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return;
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
