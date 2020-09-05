ad1479c5b7e8536eb4c8ce1af482f0788ba6f9e0
apla02 <1873@holbertonschool.com>
2020-09-04 22:12:38 -0500

    insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,41 @@
 #include "lists.h"
 /**
- * get_dnodeint_at_index - gets a node from a dlistint_t list.
- * @head: double pointer to structure
- * @index: index of node to get from the list
+ * insert_dnodeint_at_index - insert  a node to a dlistint_t list.
+ * @h: double pointer to structure
+ * @idx: index of node to get from the list
+ * @n: data
  * Return: the adrress of nth node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new, *tmp;
-    unsigned int nth;
+	dlistint_t *new, *tmp;
+	unsigned int nth;
 
-    new = (dlistint_t *)malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = n;
-    tmp = *h;
-    if (idx == 0)
-    {
-        new->n = n; /*asigno el valor a n*/
-        new->next = (*h);/* el new node debe cont la dir del que er el 1 elemento*/
-        new->prev = NULL;
-        if (*h)
-            (*h)->prev = new;
-        (*h) = new;
-        return (new);
-    }
+	new = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	tmp = *h;
+	if (idx == 0)
+	{
+		new->n = n;
+		new->next = (*h);
+		if (*h)
+			(*h)->prev = new;
+		(*h) = new;
+		return (new);
+	}
 
 	nth = 0;
-	while (tmp && nth < idx -1)
+	while (tmp && nth < idx - 1)
 	{
-        nth++;
+		nth++;
 		tmp = tmp->next;
 	}
 
-    new->next = tmp->next;
-    new->prev = tmp;
-    tmp->next = new;
-    
+	new->next = tmp->next;
+	new->prev = tmp;
+	tmp->next = new;
+
 	return (new);
 }
