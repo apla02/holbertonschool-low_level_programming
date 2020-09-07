a83ce3257726a43ca04bfe76ec8fc5fe49669ee4
apla02 <1873@holbertonschool.com>
2020-09-07 09:50:05 -0500

    library for python

---
+++ 0x18-dynamic_libraries/100-main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * add - adds two integers
+ * @a: integer
+ * @b: integer
+ * Return: no zero
+ */
+int add(int a, int b) {return (a + b); }
+
+/**
+ * sub - substract two integers
+ * @a: int
+ * @b: int
+ * Return: no zero
+ */
+int sub(int a, int b) {return (a - b); }
+
+/**
+ * mul - multiply two integers
+ * @a: int
+ * @b: int
+ * Return: no zero
+*/
+
+int mul(int a, int b) {return (a * b); }
+/**
+ * div - divide two integers
+ * @a: int
+ * @b: int
+ * Return: no zero
+ */
+int div(int a, int b) {return (a / b); }
+
+/**
+ * mod - remind between two integers
+ * @a: int
+ * @b: int
+ * Return: no zero
+ */
+int mod(int a, int b) {return (a % b); }
